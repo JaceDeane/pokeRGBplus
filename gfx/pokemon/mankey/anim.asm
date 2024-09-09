24c14018a4a74acf936202f78cda300f6be77e6f
JaceDeane <44044160+JaceDeane@users.noreply.github.com>
2024-09-09 18:38:48 +0930

    New Sprites for Mankey & Weedle
    Credit to Jace

--- gfx/pokemon/mankey/anim.asm
+++ gfx/pokemon/mankey/anim.asm
@@ -1,6 +1,1 @@
-	frame 1, 24
-	setrepeat 3
-	frame 0, 08
-	frame 2, 08
-	dorepeat 2
 	endanim
--- gfx/pokemon/weedle/anim.asm
+++ gfx/pokemon/weedle/anim.asm
@@ -1,9 +1,1 @@
-	frame 0, 06
-	frame 1, 18
-	setrepeat 2
-	frame 0, 06
-	frame 3, 06
-	frame 4, 06
-	frame 3, 06
-	dorepeat 3
 	endanim
