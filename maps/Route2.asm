b70885fe894fded0130bf5c8ca3ab9c3b763b058
JaceDeane <44044160+JaceDeane@users.noreply.github.com>
2024-10-26 22:02:29 +1030

    Remove the Gen II Trainers from Route 2
    Commented out

--- maps/Route2.asm
+++ maps/Route2.asm
@@ -1,7 +1,7 @@
 	object_const_def
-	const ROUTE2_BUG_CATCHER1
-	const ROUTE2_BUG_CATCHER2
-	const ROUTE2_BUG_CATCHER3
+	; const ROUTE2_BUG_CATCHER1
+	; const ROUTE2_BUG_CATCHER2
+	; const ROUTE2_BUG_CATCHER3
 	const ROUTE2_POKE_BALL1
 	const ROUTE2_POKE_BALL2
 	const ROUTE2_POKE_BALL3
@@ -13,38 +13,38 @@ Route2_MapScripts:
 
 	def_callbacks
 
-TrainerBugCatcherRob:
-	trainer BUG_CATCHER, ROB, EVENT_BEAT_BUG_CATCHER_ROB, BugCatcherRobSeenText, BugCatcherRobBeatenText, 0, .Script
+; TrainerBugCatcherRob:
+	; trainer BUG_CATCHER, ROB, EVENT_BEAT_BUG_CATCHER_ROB, BugCatcherRobSeenText, BugCatcherRobBeatenText, 0, .Script
 
-.Script:
-	endifjustbattled
-	opentext
-	writetext BugCatcherRobAfterBattleText
-	waitbutton
-	closetext
-	end
+; .Script:
+	; endifjustbattled
+	; opentext
+	; writetext BugCatcherRobAfterBattleText
+	; waitbutton
+	; closetext
+	; end
 
-TrainerBugCatcherEd:
-	trainer BUG_CATCHER, ED, EVENT_BEAT_BUG_CATCHER_ED, BugCatcherEdSeenText, BugCatcherEdBeatenText, 0, .Script
+; TrainerBugCatcherEd:
+	; trainer BUG_CATCHER, ED, EVENT_BEAT_BUG_CATCHER_ED, BugCatcherEdSeenText, BugCatcherEdBeatenText, 0, .Script
 
-.Script:
-	endifjustbattled
-	opentext
-	writetext BugCatcherEdAfterBattleText
-	waitbutton
-	closetext
-	end
+; .Script:
+	; endifjustbattled
+	; opentext
+	; writetext BugCatcherEdAfterBattleText
+	; waitbutton
+	; closetext
+	; end
 
-TrainerBugCatcherDoug:
-	trainer BUG_CATCHER, DOUG, EVENT_BEAT_BUG_CATCHER_DOUG, BugCatcherDougSeenText, BugCatcherDougBeatenText, 0, .Script
+; TrainerBugCatcherDoug:
+	; trainer BUG_CATCHER, DOUG, EVENT_BEAT_BUG_CATCHER_DOUG, BugCatcherDougSeenText, BugCatcherDougBeatenText, 0, .Script
 
-.Script:
-	endifjustbattled
-	opentext
-	writetext BugCatcherDougAfterBattleText
-	waitbutton
-	closetext
-	end
+; .Script:
+	; endifjustbattled
+	; opentext
+	; writetext BugCatcherDougAfterBattleText
+	; waitbutton
+	; closetext
+	; end
 
 Route2Sign:
 	jumptext Route2SignText
@@ -79,59 +79,59 @@ Route2HiddenFullRestore:
 Route2HiddenRevive:
 	hiddenitem REVIVE, EVENT_ROUTE_2_HIDDEN_REVIVE
 
-BugCatcherRobSeenText:
-	text "My bug #MON are"
-	line "tough. Prepare to"
-	cont "lose!"
-	done
+; BugCatcherRobSeenText:
+	; text "My bug #MON are"
+	; line "tough. Prepare to"
+	; cont "lose!"
+	; done
 
-BugCatcherRobBeatenText:
-	text "I was whipped…"
-	done
+; BugCatcherRobBeatenText:
+	; text "I was whipped…"
+	; done
 
-BugCatcherRobAfterBattleText:
-	text "I'm going to look"
-	line "for stronger bug"
-	cont "#MON."
-	done
+; BugCatcherRobAfterBattleText:
+	; text "I'm going to look"
+	; line "for stronger bug"
+	; cont "#MON."
+	; done
 
-BugCatcherEdSeenText:
-	text "If you walk in"
-	line "tall grass wearing"
+; BugCatcherEdSeenText:
+	; text "If you walk in"
+	; line "tall grass wearing"
 
-	para "shorts, do you get"
-	line "nicks and cuts?"
-	done
+	; para "shorts, do you get"
+	; line "nicks and cuts?"
+	; done
 
-BugCatcherEdBeatenText:
-	text "Ouch, ouch, ouch!"
-	done
+; BugCatcherEdBeatenText:
+	; text "Ouch, ouch, ouch!"
+	; done
 
-BugCatcherEdAfterBattleText:
-	text "They'll really"
-	line "sting when you"
-	cont "take a bath."
-	done
+; BugCatcherEdAfterBattleText:
+	; text "They'll really"
+	; line "sting when you"
+	; cont "take a bath."
+	; done
 
-BugCatcherDougSeenText:
-	text "Why don't girls"
-	line "like bug #MON?"
-	done
+; BugCatcherDougSeenText:
+	; text "Why don't girls"
+	; line "like bug #MON?"
+	; done
 
-BugCatcherDougBeatenText:
-	text "No good!"
-	done
+; BugCatcherDougBeatenText:
+	; text "No good!"
+	; done
 
-BugCatcherDougAfterBattleText:
-	text "Bug #MON squish"
-	line "like plush toys"
+; BugCatcherDougAfterBattleText:
+	; text "Bug #MON squish"
+	; line "like plush toys"
 
-	para "when you squeeze"
-	line "their bellies."
+	; para "when you squeeze"
+	; line "their bellies."
 
-	para "I love how they"
-	line "feel!"
-	done
+	; para "I love how they"
+	; line "feel!"
+	; done
 
 Route2SignText:
 	text "ROUTE 2"
@@ -148,26 +148,26 @@ Route2_MapEvents:
 	db 0, 0 ; filler
 
 	def_warp_events
-	warp_event 15, 15, ROUTE_2_NUGGET_HOUSE, 1
-	warp_event 15, 31, ROUTE_2_GATE, 3
-	warp_event 16, 27, ROUTE_2_GATE, 1
-	warp_event 17, 27, ROUTE_2_GATE, 2
-	warp_event 12,  7, DIGLETTS_CAVE, 3
+	warp_event 15, 19, ROUTE_2_NUGGET_HOUSE, 1
+	warp_event 15, 39, ROUTE_2_GATE, 3
+	warp_event 16, 35, ROUTE_2_GATE, 1
+	warp_event 17, 35, ROUTE_2_GATE, 2
+	warp_event 12,  9, DIGLETTS_CAVE, 3
 
 	def_coord_events
 
 	def_bg_events
-	bg_event  7, 51, BGEVENT_READ, Route2Sign
-	bg_event 11,  9, BGEVENT_READ, Route2DiglettsCaveSign
+	bg_event  5, 65, BGEVENT_READ, Route2Sign
+	bg_event 11, 11, BGEVENT_READ, Route2DiglettsCaveSign
 	bg_event  7, 23, BGEVENT_ITEM, Route2HiddenMaxEther
 	bg_event  4, 14, BGEVENT_ITEM, Route2HiddenFullHeal
 	bg_event  4, 27, BGEVENT_ITEM, Route2HiddenFullRestore
 	bg_event 11, 30, BGEVENT_ITEM, Route2HiddenRevive
 
 	def_object_events
-	object_event 10, 45, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 5, TrainerBugCatcherRob, -1
-	object_event  6,  4, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_SPINCLOCKWISE, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 3, TrainerBugCatcherEd, -1
-	object_event  0, 40, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 3, TrainerBugCatcherDoug, -1
+	; object_event 10, 45, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 5, TrainerBugCatcherRob, -1
+	; object_event  6,  4, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_SPINCLOCKWISE, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 3, TrainerBugCatcherEd, -1
+	; object_event  0, 40, SPRITE_BUG_CATCHER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_TRAINER, 3, TrainerBugCatcherDoug, -1
 	object_event  0, 29, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route2DireHit, EVENT_ROUTE_2_DIRE_HIT
 	object_event  2, 23, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route2MaxPotion, EVENT_ROUTE_2_MAX_POTION
 	object_event 19,  2, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route2Carbos, EVENT_ROUTE_2_CARBOS
