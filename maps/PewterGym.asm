bdc96caa6ab1eb79ff7a471735fc8a9cf5d5edd5
JaceDeane <44044160+JaceDeane@users.noreply.github.com>
2025-05-06 21:45:15 +0930

    Update PewterGym.asm
    TODO: Replace TM handed out with an updated one

--- maps/PewterGym.asm
+++ maps/PewterGym.asm
@@ -27,14 +27,29 @@ PewterGymBrockScript:
 	playsound SFX_GET_BADGE
 	waitsfx
 	setflag ENGINE_BOULDERBADGE
+	; clearevent EVENT_ROUTE22_RIVAL_WANTS_BATTLE
+	setmapscene ROUTE_22, SCENE_ROUTE22_NOOP ; No longer able to fight Rival on Route 22
 	writetext BrockBoulderBadgeText
+	waitbutton
+.FightDone:
+	checkevent EVENT_GOT_TM31_MUD_SLAP ; TODO -- New TM
+	iftrue .SpeechAfterTM
+	writetext BrockWaitTakeThisText
+	promptbutton
+	verbosegiveitem TM_MUD_SLAP ; TODO -- New TM
+	iffalse .NoRoomForTM
+	setevent EVENT_GOT_TM31_MUD_SLAP ; TODO -- New TM
+	writetext BrockTMExplanationText
+	waitbutton
+	writetext BrockFightDoneText
 	waitbutton
 	closetext
 	end
 
-.FightDone:
+.SpeechAfterTM:
 	writetext BrockFightDoneText
 	waitbutton
+.NoRoomForTM:
 	closetext
 	end
 
@@ -55,6 +70,17 @@ PewterGymGuideScript:
 	checkevent EVENT_BEAT_BROCK
 	iftrue .PewterGymGuideWinScript
 	writetext PewterGymGuideText
+	yesorno
+	iffalse .PewterGymGuideDeclinedScript
+	writetext PewterGymGuideBeginAdviceText
+	waitbutton
+	writetext PewterGymGuideAdviceText
+	waitbutton
+	closetext
+	end
+	
+.PewterGymGuideDeclinedScript:
+	writetext PewterGymGuideFreeServiceText
 	waitbutton
 	closetext
 	end
@@ -74,136 +100,168 @@ PewterGymStatue:
 	jumpstd GymStatue2Script
 
 BrockIntroText:
-	text "BROCK: Wow, it's"
-	line "not often that we"
+	text "I'm BROCK!"
+	line "I'm PEWTER's GYM"
+	cont "LEADER!"
 
-	para "get a challenger"
-	line "from JOHTO."
+	para "I believe in rock-"
+	line "hard defense and"
+	cont "determination!"
 
-	para "I'm BROCK, the"
-	line "PEWTER GYM LEADER."
+	para "That's why my"
+	line "#MON are all"
+	cont "the rock-type!"
 
-	para "I'm an expert on"
-	line "rock-type #MON."
-
-	para "My #MON are im-"
-	line "pervious to most"
-
-	para "physical attacks."
-	line "You'll have a hard"
-
-	para "time inflicting"
-	line "any damage."
-
-	para "Come on!"
+	para "Do you still want"
+	line "to challenge me?"
+	
+	para "Fine then! Show"
+	line "me your best!"
 	done
 
 BrockWinLossText:
-	text "BROCK: Your #-"
-	line "MON's powerful at-"
-	cont "tacks overcame my"
-	cont "rock-hard defense…"
+	text "BROCK: I took"
+	line "you for granted."
 
-	para "You're stronger"
-	line "than I expected…"
-
-	para "Go ahead--take"
-	line "this BADGE."
+	para "As proof of your"
+	line "victory, here's"
+	cont "the BOULDERBADGE!"
 	done
 
 ReceivedBoulderBadgeText:
 	text "<PLAYER> received"
-	line "BOULDERBADGE."
+	line "the BOULDERBADGE!"
 	done
 
 BrockBoulderBadgeText:
-	text "BROCK: <PLAY_G>,"
-	line "thanks. I enjoyed"
+	text "That's an official"
+	line "#MON LEAGUE"
+	cont "BADGE!"
 
-	para "battling you, even"
-	line "though I am a bit"
-	cont "upset."
+	para "Its bearer's"
+	line "#MON become"
+	cont "more powerful!"
 
-	para "That BOULDERBADGE"
-	line "will make your"
+	para "The technique"
+	line "FLASH can now be"
+	cont "used any time!"
+	done
+	
+BrockWaitTakeThisText:
+	text "Wait! Take this"
+	line "with you!"
+	done
 
-	para "#MON even more"
-	line "powerful."
+BrockTMExplanationText:
+	text "A TM contains a"
+	line "technique that"
+	cont "can be taught to"
+	cont "#MON!"
+
+	para "A TM is good only"
+	line "once! So when you"
+	cont "use one to teach"
+	
+	para "a new technique,"
+	line "pick the #MON"
+	cont "carefully!"
+
+	; para "TM34 contains"
+	; line "BIDE!"
+
+	; para "Your #MON will"
+	; line "absorb damage in"
+	; cont "battle, then pay"
+	; cont "it back double!"
+	
+	para "TM31 contains" ; TEMP TEXT
+	line "MUD-SLAP."
+
+	para "It reduces the"
+	line "enemy's accuracy"
+
+	para "while it causes"
+	line "damage."
 	done
 
 BrockFightDoneText:
-	text "BROCK: The world"
-	line "is huge. There are"
+	text "There are all"
+	line "kinds of trainers"
+	cont "in the world!"
 
-	para "still many strong"
-	line "trainers like you."
+	para "You appear to be"
+	line "very gifted as a"
+	cont "#MON trainer!"
 
-	para "Just wait and see."
-	line "I'm going to be-"
-	cont "come a lot strong-"
-	cont "er too."
+	para "Go to the GYM in"
+	line "CERULEAN and test"
+	cont "your abilities!"
 	done
 
 CamperLiamSeenText:
-	text "The trainers of"
-	line "this GYM use rock-"
-	cont "type #MON."
+	text "Stop right there,"
+	line "kid!"
 
-	para "The rock-type has"
-	line "high DEFENSE."
-
-	para "Battles could end"
-	line "up going a long"
-
-	para "time. Are you"
-	line "ready for this?"
+	para "You're still light"
+	line "years from facing"
+	cont "BROCK!"
 	done
 
 CamperLiamBeatenText:
-	text "I have to win"
-	line "these battles…"
+	text "Darn!"
+
+	para "Light years isn't"
+	line "time! It measures"
+	cont "distance!"
 	done
 
 CamperLiamAfterBattleText:
-	text "Hey, you! Trainer"
-	line "from JOHTO! BROCK"
-
-	para "is tough. He'll"
-	line "punish you if you"
-
-	para "don't take him"
-	line "seriously."
+	text "You're pretty hot,"
+	line "but not as hot"
+	cont "as BROCK!"
 	done
 
 PewterGymGuideText:
-	text "Yo! CHAMP in"
-	line "making! You're"
+	text "Hiya! I can tell"
+	line "you have what it"
+	cont "takes to become a"
+	cont "#MON champ!"
 
-	para "really rocking."
-	line "Are you battling"
+	para "I'm no trainer,"
+	line "but I can tell"
+	cont "you how to win!"
 
-	para "the GYM LEADERS of"
-	line "KANTO?"
+	para "Let me take you"
+	line "to the top!"
+	done
 
-	para "They're strong and"
-	line "dedicated people,"
+PewterGymGuideBeginAdviceText:
+	text "All right! Let's"
+	line "get happening!"
+	done
 
-	para "just like JOHTO's"
-	line "GYM LEADERS."
+PewterGymGuideAdviceText:
+	text "The 1st #MON"
+	line "out in a match is"
+	cont "at the top of the"
+	cont "#MON LIST!"
+
+	para "By changing the"
+	line "order of #MON,"
+	cont "matches could be"
+	cont "made easier!"
+	done
+
+PewterGymGuideFreeServiceText:
+	text "It's a free"
+	line "service! Let's"
+	cont "get happening!"
 	done
 
 PewterGymGuideWinText:
-	text "Yo! CHAMP in"
-	line "making! That GYM"
-
-	para "didn't give you"
-	line "much trouble."
-
-	para "The way you took"
-	line "charge was really"
-
-	para "inspiring. I mean"
-	line "that seriously."
+	text "Just as I thought!"
+	line "You're #MON"
+	cont "champ material!"
 	done
 
 PewterGym_MapEvents:
