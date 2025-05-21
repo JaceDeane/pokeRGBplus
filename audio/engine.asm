a7d0cfb6ee1f2b46aba81549959051c179ddb5df
JaceDeane <44044160+JaceDeane@users.noreply.github.com>
2025-05-21 10:04:45 +0930

    Fix ReadNoiseSample Affecting RBY Music
    (https://github.com/pret/pokecrystal/wiki/Fix-ReadNoiseSample-'inc-a'-distorting-channel-4-percussion)

--- audio/engine.asm
+++ audio/engine.asm
@@ -1116,7 +1116,7 @@ ReadNoiseSample:
 	jr z, .quit
 
 	and $f
-	inc a
+	; inc a ; RBY Music Fix -- TODO -- Remove when not using Gen I music anymore
 	ld [wNoiseSampleDelay], a
 	ld a, [de]
 	inc de
