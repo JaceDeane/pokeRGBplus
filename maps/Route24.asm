31304f12c1ce48d799b3a1c80f54dad724cdbe31
JaceDeane <44044160+JaceDeane@users.noreply.github.com>
2025-05-28 13:35:47 +0930

    Finish Route 24 Events

--- maps/Route24.asm
+++ maps/Route24.asm
@@ -18,135 +18,44 @@ Route24_MapScripts:
 Route24NoopScene:
 	end
 
-Route24Scene2:
-	;disappear ROUTE24_COOLTRAINER_M1 ; Ensures the Rocket Recruiter disappears
+Route24CooltrainerM1Event:
+	turnobject PLAYER, RIGHT
+Route24CooltrainerM1Script:
+	faceplayer
+	opentext
+	checkevent EVENT_BEAT_ROCKET_GRUNTM_6
+	iftrue .beaten_grunt
+	checkevent EVENT_GOT_NUGGET
+	iftrue .got_item
+	writetext Route24CooltrainerM1YouBeatOurContestText
+	waitbutton
+	writetext Route24CooltrainerM1YouJustEarnedAPrizeText
+	promptbutton
+	verbosegiveitem NUGGET
+	iffalse .bag_full
+	setevent EVENT_GOT_NUGGET
+.got_item
+	writetext Route24CooltrainerM1JoinTeamRocketText
+	playmusic MUSIC_EVIL_TRAINER_ENCOUNTER
+	waitbutton
+	closetext
+	winlosstext Route24CooltrainerM1DefeatedText, -1
+	loadtrainer GRUNTM, GRUNTM_6
+	startbattle
+	reloadmapafterbattle
+	setevent EVENT_BEAT_ROCKET_GRUNTM_6
+	setscene SCENE_ROUTE24_ROCKET_DEFEATED
+	; fallthrough
+.beaten_grunt
+	writetext Route24CooltrainerM1YouCouldBecomeATopLeaderText
+	waitbutton
+	closetext
 	end
-
-; Route24RocketScript:
-	; faceplayer
-	; playmusic MUSIC_ROCKET_ENCOUNTER
-	; opentext
-	; writetext Route24RocketSeenText
-	; waitbutton
-	; closetext
-	; winlosstext Route24RocketBeatenText, -1
-	; loadtrainer GRUNTM, GRUNTM_31
-	; startbattle
-	; dontrestartmapmusic
-	; reloadmapafterbattle
-	; playmusic MUSIC_ROCKET_ENCOUNTER
-	; opentext
-	; writetext Route24RocketAfterBattleText
-	; promptbutton
-	; special FadeOutMusic
-	; writetext Route24RocketDisappearsText
-	; waitbutton
-	; closetext
-	; special FadeOutToBlack
-	; special ReloadSpritesNoPalettes
-	; disappear ROUTE24_ROCKET
-	; pause 25
-	; special FadeInFromBlack
-	; playmapmusic
-	; end
-
-; Route24CooltrainerM1Text:
-	; text_asm
-	; ResetEvent EVENT_NUGGET_REWARD_AVAILABLE
-	; CheckEvent EVENT_GOT_NUGGET
-	; jr nz, .got_item
-	; ld hl, .YouBeatOurContestText
-	; call PrintText
-	; lb bc, NUGGET, 1
-	; call GiveItem
-	; jr nc, .bag_full
-	; SetEvent EVENT_GOT_NUGGET
-	; ld hl, .ReceivedNuggetText
-	; call PrintText
-	; ld hl, .JoinTeamRocketText
-	; call PrintText
-	; ld hl, wStatusFlags3
-	; set BIT_TALKED_TO_TRAINER, [hl]
-	; set BIT_PRINT_END_BATTLE_TEXT, [hl]
-	; ld hl, .DefeatedText
-	; ld de, .DefeatedText
-	; call SaveEndBattleTextPointers
-	; ldh a, [hSpriteIndex]
-	; ld [wSpriteIndex], a
-	; call EngageMapTrainer
-	; call InitBattleEnemyParameters
-	; xor a
-	; ldh [hJoyHeld], a
-	; ld a, SCRIPT_ROUTE24_AFTER_ROCKET_BATTLE
-	; ld [wRoute24CurScript], a
-	; ld [wCurMapScript], a
-	; jp TextScriptEnd
-; .got_item
-	; ld hl, .YouCouldBecomeATopLeaderText
-	; call PrintText
-	; jp TextScriptEnd
-; .bag_full
-	; ld hl, .NoRoomText
-	; call PrintText
-	; SetEvent EVENT_NUGGET_REWARD_AVAILABLE
-	; jp TextScriptEnd
-
-; .YouBeatOurContestText:
-	; text_far _Route24CooltrainerM1YouBeatOurContestText
-	; sound_get_item_1
-	; text_far _Route24CooltrainerM1YouJustEarnedAPrizeText
-	; text_end
-
-; .ReceivedNuggetText:
-	; text_far _Route24CooltrainerM1ReceivedNuggetText
-	; sound_get_item_1
-	; text_promptbutton
-	; text_end
-
-; .NoRoomText:
-	; text_far _Route24CooltrainerM1NoRoomText
-	; text_end
-
-; .JoinTeamRocketText:
-	; text_far _Route24CooltrainerM1JoinTeamRocketText
-	; text_end
-
-; .DefeatedText:
-	; text_far _Route24CooltrainerM1DefeatedText
-	; text_end
-
-; .YouCouldBecomeATopLeaderText:
-	; text_far _Route24CooltrainerM1YouCouldBecomeATopLeaderText
-	; text_end
-
-; Route24AfterRocketBattleScript:
-	; ld a, [wIsInBattle]
-	; cp $ff
-	; jp z, Route24SetDefaultScript
-	; call UpdateSprites
-	; ld a, D_RIGHT | D_LEFT | D_UP | D_DOWN
-	; ld [wJoyIgnore], a
-	; SetEvent EVENT_BEAT_ROUTE24_ROCKET
-	; ld a, TEXT_ROUTE24_COOLTRAINER_M1
-	; ldh [hTextID], a
-	; call DisplayTextID
-	; xor a
-	; ld [wJoyIgnore], a
-	; ld a, SCRIPT_ROUTE24_DEFAULT
-	; ld [wRoute24CurScript], a
-	; ld [wCurMapScript], a
-	; ret
-
-; Trainer***___:
-	; trainer BUG_CATCHER, COLTON, EVENT_BEAT_BUG_CATCHER_COLTON, BugCatcherColtonSeenText, BugCatcherColtonBeatenText, 0, .Script
-
-; .Script:
-	; endifjustbattled
-	; opentext
-	; writetext BugCatcherColtonAfterBattleText
-	; waitbutton
-	; closetext
-	; end
+.bag_full
+	writetext Route24CooltrainerM1NoRoomText
+	waitbutton
+	closetext
+	end
 
 TrainerCamperShane:
 	trainer CAMPER, SHANE, EVENT_BEAT_CAMPER_SHANE, Route24CooltrainerM2BattleText, Route24CooltrainerM2EndBattleText, 0, .Script
@@ -227,13 +136,6 @@ Route24CooltrainerM1YouJustEarnedAPrizeText:
 	text "You just earned a"
 	line "fabulous prize!"
 	done
-
-; Route24CooltrainerM1ReceivedNuggetText:
-	; text "<PLAYER> received"
-	; line "a @"
-	; text_ram wStringBuffer
-	; text "!@"
-	; text_end
 
 Route24CooltrainerM1NoRoomText:
 	text "You don't have"
@@ -378,13 +280,12 @@ Route24_MapEvents:
 	def_warp_events
 
 	def_coord_events
-	coord_event 10, 15, SCENE_ROUTE24_NOOP, ObjectEvent ; Route24...CooltrainerM1/Rocket...Script
+	coord_event 10, 15, SCENE_ROUTE24_NOOP, Route24CooltrainerM1Event
 
 	def_bg_events
 
 	def_object_events
-	; object_event  9, 15, SPRITE_ROCKET, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route24RocketScript, EVENT_ROUTE_24_ROCKET
-	object_event 11, 15, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_ROUTE_24_ROCKET ; Route24Rocket6Event
+	object_event 11, 15, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, 0, OBJECTTYPE_SCRIPT, 0, Route24CooltrainerM1Script, -1
 	object_event  5, 20, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_UP, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 4, TrainerCamperShane, -1
 	object_event 11, 19, SPRITE_COOLTRAINER_M, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerCamperEthan, -1
 	object_event 10, 22, SPRITE_LASS, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerLassReli, -1
@@ -392,4 +293,3 @@ Route24_MapEvents:
 	object_event 10, 28, SPRITE_LASS, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, 0, OBJECTTYPE_TRAINER, 1, TrainerLassAli, -1
 	object_event 11, 31, SPRITE_YOUNGSTER, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_TRAINER, 1, TrainerBugCatcherCale, -1
 	object_event 10,  5, SPRITE_POKE_BALL, SPRITEMOVEDATA_STILL, 0, 0, -1, -1, 0, OBJECTTYPE_ITEMBALL, 0, Route24ThunderWaveTM, EVENT_GOT_TM49_FURY_CUTTER ; TODO -- THUNDER_WAVE
-	;Picnicker
